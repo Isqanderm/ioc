6ed269233b8bfa519c8e2a6e0d7ab7330a321303
IsqanderM <aleksandr.melnik.personal@gmail.com>
2024-10-16 22:00:19 +0200

    fix(ioc): rename nsModule file

--- packages/ioc/src/index.ts
+++ packages/ioc/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./interfaces";
-export { NsModule } from "./decorators/NsModule";
+export { NsModule } from "./decorators/nsModule";
 export { Inject } from "./decorators/inject";
 export { Injectable } from "./decorators/injectable";
 export { Global } from "./decorators/global";
