92c18be2e80a9f54fbf434f34c8325f9cad6f14c
Aleksandr Melnik <152389998+Isqanderm@users.noreply.github.com>
2026-09-13 01:22:01 +0200

    chore(type-checker): remove unused cycle fixture file

--- packages/type-checker/__test__/fixtures/cycle-a.ts
+++
@@ -1,7 +0,0 @@
-import { Inject as Dependency, Injectable as Service } from "@nexus-ioc/core";
-import { CycleB } from "./cycle-b";
-
-@Service()
-export class CycleA {
-  constructor(@Dependency(CycleB) cycleB: CycleB) {}
-}
