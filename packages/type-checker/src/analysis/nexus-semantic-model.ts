e62155c18c073d9a732feb08a23517e72df0695c
Aleksandr Melnik <152389998+Isqanderm@users.noreply.github.com>
2026-09-13 00:30:50 +0200

    refactor(type-checker): introduce NexusClass semantic model

--- packages/type-checker/src/analysis/nexus-semantic-model.ts
+++ packages/type-checker/src/analysis/nexus-semantic-model.ts
@@ -12,30 +12,28 @@ export type NexusToken =
 			kind: "string";
 			value: string;
 			source: NexusSourceSpan;
-	  }
+		}
 	| {
 			kind: "symbol";
 			declaration?: ts.Symbol;
 			source: NexusSourceSpan;
-	  }
+		}
 	| {
 			kind: "reference";
 			symbol: ts.Symbol;
 			source: NexusSourceSpan;
-	  }
+		}
 	| {
 			kind: "expression";
 			source: NexusSourceSpan;
-	  };
+		};
 
 export type NexusDependency = {
 	location: "constructor" | "property";
-	parameterName: string;
-	parameterType?: ts.TypeNode;
+	name: string;
 	token?: NexusToken;
 	optional: boolean;
 	source: NexusSourceSpan;
-	declaration: ts.ParameterDeclaration | ts.PropertyDeclaration;
 };
 
 export type NexusDecoratorKind =
@@ -47,17 +45,15 @@ export type NexusDecoratorKind =
 
 export type NexusDecorator = {
 	kind: NexusDecoratorKind;
-	declaration: ts.Decorator;
-	expression: ts.Expression;
 	source: NexusSourceSpan;
 };
 
-export type NexusClassModel = {
-	node: ts.ClassDeclaration;
+export type NexusClass = {
 	name?: string;
-	decorators: NexusDecorator[];
+	source: NexusSourceSpan;
+	decorators: readonly NexusDecorator[];
+	dependencies: readonly NexusDependency[];
 	isInjectable: boolean;
 	isModule: boolean;
 	isGlobal: boolean;
-	dependencies: NexusDependency[];
 };
